f907f20ec9f0cd15fcd5d4346e78757be29461e4
kmoilane <karri.moilanen@gmail.com>
2023-06-26 14:48:42 +0300

    Added the option to load a pet from the file

--- includes/utils.h
+++ includes/utils.h
@@ -9,5 +9,6 @@ int validate_input(std::string str, int min, int max);
 bool validate_file_name(std::string& file_name);
 bool file_exists(std::string& file_name);
 int create_save_file(Pet& pet, std::string& file_name);
+std::string select_save();
 
 #endif
