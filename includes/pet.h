5948f46bc1e681c92ab6065b0852ec648e2c9d0d
kmoilane <karri.moilanen@gmail.com>
2023-06-26 11:38:53 +0300

    Added feeding option for the pet and happiness + fullness to the struct

--- includes/pet.h
+++ includes/pet.h
@@ -2,13 +2,20 @@
 #define PET_H
 
 #include <string>
+#include <iostream>
+
+constexpr std::pair<int,int> FULLNESS {0, 100};
+constexpr std::pair<int,int> HAPPINESS {0, 10};
 
 struct Pet
 {
     std::string name {};
     std::string type {};
+    int         happiness {2};  //Every pet starts with happiness of 2
+    int         fullness {30};  //Every pet starts with fullness of 30
 };
 
 Pet create_new_pet(std::string name, std::string type);
+void feed_pet(Pet& pet);
 
 #endif
--- src/pet.cpp
+++ src/pet.cpp
@@ -2,6 +2,25 @@
 #include <iostream>
 #include <string>
 
+void feed_pet(Pet& pet)
+{
+    if (pet.fullness == FULLNESS.second)
+        std::cout << pet.name << " is not hungry right now.\n";
+    else if (pet.fullness < FULLNESS.second)
+    {
+        if (pet.fullness + 10 >= FULLNESS.second)
+            pet.fullness = 100;
+        else
+            pet.fullness += 10;
+        std::cout << pet.name << " is now fed and it's fullness is: "
+            << pet.fullness << '\n';
+        pet.happiness += 2;
+        if (pet.happiness > 10)
+            pet.happiness = 10;
+        std::cout << pet.name << "'s happiness is now: " << pet.happiness << '\n';
+    }
+}
+
 Pet create_new_pet(std::string name, std::string type)
 {
     Pet new_pet {name, type};
